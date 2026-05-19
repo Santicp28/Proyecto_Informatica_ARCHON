f72a563d99bf6e23247e3f844326cb383637c129
juan-cerro <130755224+juan-cerro@users.noreply.github.com>
2026-05-19 12:42:19 +0200

    poco

--- includes/Interaccion.h
+++ includes/Interaccion.h
@@ -3,21 +3,29 @@
 #include "Casilla.h"
 
 class Interaccion {
+public:
     void piezaEfectoTipoCasilla(Pieza* p, Casilla c) {
 
-        if (p->tipo == OSCURO) {
-
+        if (p->bando == Bando::LUZ) {
             switch (c.getTipo()) {
+            case TipoCasilla::CLARA:
+                p->fuerza *= 1.2;
+                break;
             case TipoCasilla::OSCURA:
                 p->fuerza *= 0.8;
                 break;
-
-            case TipoCasilla::CLARA:
+                //poner cada caso
+            }
+        }
+        else {
+            switch (c.getTipo()) {
+            case TipoCasilla::OSCURA:
                 p->fuerza *= 1.2;
-				break;
-
-
-
+                break;
+            case TipoCasilla::CLARA:
+                p->fuerza *= 0.8;
+                break;
+                //igual
             }
         }
     }
