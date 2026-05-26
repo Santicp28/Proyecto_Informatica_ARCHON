f0e8b42e94ad4642e4db5ebab88aeeb72d7c7f6c
AdJuG <adjuarezgil@gmail.com>
2026-05-26 18:45:41 +0200

    arreglo rama

---
+++ Grafhechizos.h
@@ -0,0 +1,13 @@
+#pragma once
+struct grafhechizo {
+    const char* graf;
+};
+
+inline constexpr grafhechizo cancelar = { "assets/Graficos/cease.png" };
+inline constexpr grafhechizo elementos = { "assets/Graficos/elemental.png" };
+inline constexpr grafhechizo intercambio = { "assets/Graficos/exchange.png" };
+inline constexpr grafhechizo explosion = { "assets/Graficos/explosion.png" };
+inline constexpr grafhechizo curar = { "assets/Graficos/heal.png" };
+inline constexpr grafhechizo revivir = { "assets/Graficos/revive.png" };
+inline constexpr grafhechizo tiempo = { "assets/Graficos/shifttime.png" };
+inline constexpr grafhechizo teletransportar = { "assets/Graficos/teleport.png" };
--- Graftablero.h
+++ Graftablero.h
@@ -3,7 +3,13 @@ struct graftablero {
     const char* graf;
 };
 
-inline constexpr graftablero fondotablero = { "bin/Graficos/fondo.png" };
-inline constexpr graftablero luz1 = { "bin/Graficos/.png" };
-inline constexpr graftablero luz2 = { "bin/Graficos/fondo.png" };
-inline constexpr graftablero luz3 = { "bin/Graficos/fondo.png" };
+inline constexpr graftablero fondotablero = { "assets/Graficos/fondo.png" };
+inline constexpr graftablero luz1 = { "assets/Graficos/tablero4.png" };
+inline constexpr graftablero luz2 = { "assets/Graficos/tablero5.png" };
+inline constexpr graftablero luz3 = { "assets/Graficos/tablero6.png" };
+inline constexpr graftablero oscuro1 = { "assets/Graficos/tablero1.png" };
+inline constexpr graftablero oscuro2 = { "assets/Graficos/tablero2.png" };
+inline constexpr graftablero oscuro3 = { "assets/Graficos/tablero3.png" };
+inline constexpr graftablero elegido = { "assets/Graficos/elegido.png" };
+inline constexpr graftablero posiciones = { "assets/Graficos/posiciones.png" };
+inline constexpr graftablero cursor = { "assets/Graficos/Cursor.png" };
