005fcb390234aaf97248ed61af2cfc89c38fdb67
juan-cerro <130755224+juan-cerro@users.noreply.github.com>
2026-05-26 15:31:18 +0200

    sigo añadiendo cosas que no estaban

--- includes/Tipos.h
+++ includes/Tipos.h
@@ -26,14 +26,56 @@ enum class TipoMovimiento {
     TELETRANSPORTE
 };
 
+
+enum class Cadencia {
+    VARIABLE,
+    MUYRAPIDA,
+    RAPIDA,
+    PROMEDIO,
+    LENTA
+};
+enum class Velocidad {
+    VARIABLE,
+    NORMAL,
+    BAJA
+};
+enum class Vida {
+    VARIABLE,
+    MUYALTA,
+    ALTA,
+    MODERADA,
+    CORTA,
+
+};
+enum class Ataque {
+    VARIABLE,
+    MUYALTO,
+    ALTO,
+    MODERADO,
+    BAJO
+};
+enum class Velocidad_ataque {
+    VARIABLE,
+    INSTANTANEO,
+    RAPIDO,
+    NORMAL,
+    LENTO
+};
+
+enum class Rango {
+    CORTO,
+    MEDIO,
+    LARGO
+};
+
 struct PosicionMatriz {
     int fila;
     int columna;
-};
 
-inline bool operator ==(const PosicionMatriz& a, const PosicionMatriz& b) {
-    return a.fila == b.fila && a.columna == b.columna;
-}
+    bool operator==(const PosicionMatriz& p) const {
+        return fila == p.fila && columna == p.columna;
+    }
+};
 
 // PARA SABER QUE ATAQUE USAMOS
 enum class TipoAtaque {
